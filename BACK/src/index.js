2f02ecb8355793513bb5030edb6b9f37216440c8
NachoAbait <102827148+NachoAbait@users.noreply.github.com>
2023-07-19 18:16:54 -0300

    prueba token

--- BACK/src/index.js
+++ BACK/src/index.js
@@ -23,7 +23,7 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(morgan("dev"));
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "https://to-do-roan-pi.vercel.app"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
     "Access-Control-Allow-Headers",
